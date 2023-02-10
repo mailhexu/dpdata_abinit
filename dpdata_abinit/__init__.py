28bf787498e44ef33fc000f3e4d45096309fc531
hexu <mailhexu@gmail.com>
2023-02-10 11:26:22 +0100

    add histnc.py and setup.cfg

--- dpdata_abinit/__init__.py
+++ dpdata_abinit/__init__.py
@@ -1,24 +1,10 @@
 from dpdata.format import Format
-import numpy as np
+from .histnc import to_system_data
 
-@Format.register("random")
-class RandomFormat(Format):
-    def from_system(self, N, **kwargs):
-        return {
-            "atom_numbs": [20],
-            "atom_names": ['X'],
-            "atom_types": [0] * 20,
-            "cells": np.repeat(np.diag(np.diag(np.ones((3, 3))))[np.newaxis,...], N, axis=0) * 100.,
-            "coords": np.random.rand(N, 20, 3) * 100.,
-        }
+@Format.register("abihist")
+class AbihistFormat(Format):
+    def from_system(self, fname, **kwargs):
+        return None
 
-    def from_labeled_system(self, N, **kwargs):
-        return {
-            "atom_numbs": [20],
-            "atom_names": ['X'],
-            "atom_types": [0] * 20,
-            "cells": np.repeat(np.diag(np.diag(np.ones((3, 3))))[np.newaxis,...], N, axis=0) * 100.,
-            "coords": np.random.rand(N, 20, 3) * 100.,
-            "energies": np.random.rand(N) * 100.,
-            "forces": np.random.rand(N, 20, 3) * 100.,
-        }
+    def from_labeled_system(self, fname, **kwargs):
+        return to_system_data(fname)
