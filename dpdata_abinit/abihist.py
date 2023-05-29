572e705d9fc43cc5575b80da265276dbb4fdbce2
hexu <mailhexu@gmail.com>
2023-05-29 12:27:48 +0200

    add documenation

--- dpdata_abinit/abihist.py
+++ dpdata_abinit/abihist.py
@@ -10,6 +10,4 @@ class AbihistFormat(Format):
         return to_system_data(fname)
 
 
-f=AbihistFormat()
-f.from_labeled_system(fname="/home/hexu/projects/Multibinit_resources/test_max_nbody/TrainingSet.nc")    
-print(f)
+
